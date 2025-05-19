e9146cf4fae727a491d12fad0a34a483e2d1dfd0
fahad329 <kh_fahad_farooq@yahoo.com>
2025-05-19 20:16:45 +0500

    Created Data Transformation and Pickle file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from data_transformation import DataTransformation,DataTransformationConfig
+
 @dataclass
 class DataIngectionConfig:
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -48,6 +50,8 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation =DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
             
 
 
